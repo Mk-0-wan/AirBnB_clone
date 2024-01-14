5860de9c87337cd24ca0118db7502e9e661335d9
Drihmia <drihmia.redouane@gmail.com>
2024-01-14 18:44:47 +0100

    docstrings control

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,7 @@
+"""
+Create a FileStorages object and reload all object from
+the json file
+"""
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
--- tests/test_console.py
+++ tests/test_console.py
@@ -1,46 +1,78 @@
+#!/usr/bin/python3
+"""
+A test for the console class
+"""
 import unittest
-
+import pep8
 from console import HBNBCommand
 
 
 class TestHBNBCommand(unittest.TestCase):
+    """
+    A class to test the HBNBCommand class
+    """
 
     def setUp(self):
+        """
+        Set up a new HBNBCommand instance for each test
+        """
         self.cmd = HBNBCommand()
 
     def test_default(self):
-        self.cmd.default('create BaseModel')
+        """
+        Test the default prompt value
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
 
     def test_do_EOF(self):
+        """
+        Test the do_EOF method
+        """
         self.assertTrue(self.cmd.do_EOF(''))
 
     def test_do_quit(self):
+        """
+        Test the do_quit method
+        """
         self.assertTrue(self.cmd.do_quit(''))
 
     def test_do_create(self):
-        self.cmd.do_create('BaseModel')
+        """
+        Test the do_create method
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
 
     def test_do_show(self):
-        self.cmd.do_create('BaseModel')
-        self.cmd.do_show('BaseModel 1234-1234-1234')
+        """
+        Test the do_show method
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
 
     def test_do_destroy(self):
-        self.cmd.do_create('BaseModel')
-        self.cmd.do_destroy('BaseModel 1234-1234-1234')
+        """
+        Test the do_destroy method
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
 
     def test_do_all(self):
-        self.cmd.do_create('BaseModel')
-        self.cmd.do_all('BaseModel')
+        """
+        Test the do_all method
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
 
     def test_do_update(self):
-        self.cmd.do_create('BaseModel')
-        self.cmd.do_update('BaseModel 1234-1234-1234 email "aibnb@mail.com"')
+        """
+        Test the do_update method
+        """
         self.assertEqual(self.cmd.prompt, '(hbnb) ')
+
+    def test_pycodestyle(self):
+        """Testing for pycodestyle implementation"""
+        pycode_pass = pep8.StyleGuide(quite=True)
+        record = pycode_pass.check_files(
+                ['./console.py',
+                 './tests/test_console.py'])
+        self.assertEqual(record.total_errors, 0, "errors found")
 
 
 if __name__ == '__main__':
