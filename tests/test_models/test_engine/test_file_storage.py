e0467c53e1ab3d310c167324e68bfb8652d7b647
archMk <victormurithi2020@gmail.com>
2024-01-15 16:54:27 +0300

    Updated all the test cases for the class modules

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ import json
 import pep8
 import models
 import unittest
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
@@ -27,51 +28,61 @@ class TestModel(unittest.TestCase):
         """Checking if the new method is working all fine"""
         fl_obj = FileStorage()
         fl_obj._FileStorage__objects.clear()
-        # check if the dictionary __objects has been cleared.
         self.assertEqual(fl_obj._FileStorage__objects, {})
-        obj = models.user.User()
+        obj = User()
         obj.name = "victor"
         FileStorage().new(obj)
-        if(self.assertNotEqual(fl_obj._FileStorage__objects, {})):
+        if (self.assertNotEqual(fl_obj._FileStorage__objects, {})):
             key = "User." + obj.id
             self.assertIn(key, fl_obj._FileStorage__objects)
+        _x = obj
+        del FileStorage().all()[f"{_x.__class__.__name__}.{_x.id}"]
+        FileStorage().save()
 
     def test_save_method(self):
         """Checking for file saving and saving format"""
         self.assertTrue(os.path.isfile("./models/engine/file.json"))
         self.assertTrue(os.path.getsize("./models/engine/file.json"), 0)
 
-        obj = models.user.User()
+        obj = User()
         obj.name = "Tommy"
         FileStorage().save()
 
-        with open("./models/engine/file.json", mode="r") as json_file:
-            total_reads = sum(1 for _ in json_file)
+        with open("./models/engine/file.json", "r", encoding="utf-8") as jsfl:
+            total_reads = sum(1 for _ in jsfl)
         self.assertGreater(total_reads, 0)
         self.assertGreater(os.path.getsize("./models/engine/file.json"), 0)
+
+        _x = obj
+        del FileStorage().all()[f"{_x.__class__.__name__}.{_x.id}"]
+        FileStorage().save()
 
     def test_pycodestyle(self):
         """Testing for pycodestyle implementation"""
         pycode_pass = pep8.StyleGuide(quite=True)
         record = pycode_pass.check_files(
                 ['./models/engine/file_storage.py',
-                 './tests/test_models/test_file_storage.py'])
+                 './tests/test_models/test_engine/test_file_storage.py'])
         self.assertEqual(record.total_errors, 0, "errors found")
 
     def test_reload_method(self):
         """Testing for the reload methods functionality"""
-        obj = models.city.City()
+        obj = User()
         obj.name = "Birmingham"
         FileStorage().save()
         self.assertGreater(os.path.getsize("./models/engine/file.json"), 0)
-        with open("./models/engine/file.json", mode="r") as json_file:
-            red = json_file.read()
+        with open("./models/engine/file.json", "r", encoding="utf-8") as jsfl:
+            red = jsfl.read()
             if red:
                 dicts = json.loads(red)
                 self.assertEqual(type(dicts), dict)
                 test_dic = dicts[f"{type(obj).__name__}.{obj.id}"]
                 self.assertIn('name', test_dic)
                 self.assertEqual("Birmingham", test_dic['name'])
+
+        _x = obj
+        del FileStorage().all()[f"{_x.__class__.__name__}.{_x.id}"]
+        FileStorage().save()
 
 
 if __name__ == '__main__':
